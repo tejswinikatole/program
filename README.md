5e8d28953de4b63aaf746e62dd04bd690c52babf
tejswinikatole <87907268+tejswinikatole@users.noreply.github.com>
2021-12-27 22:16:21 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# mycaptian
+xyz
---
+++ main.c
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+     FILE *fp;
+      char ch;
+      int i,pos;
+      fp=fopen("input.txt","r");
+      if(fp==NULL)
+      {
+            printf("File does not exist..");
+      }
+      fseek(fp,0,SEEK_END);
+      pos=ftell(fp);
+      //printf("Current postion is %d\n",pos);
+      i=0;
+      while(i<pos)
+       {
+            i++;
+            fseek(fp,-i,SEEK_END);
+            //printf("%c",fgetc(fp));
+            ch=fgetc(fp);
+            printf("%c",ch);
+      }
+
+    return 0;
+}
